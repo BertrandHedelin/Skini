6c009f1d2914cb0e9b9a3bf2a32f0a126df537e1
Bertrand Petit-Hédelin <bertrand@hedelin.fr>
2024-03-12 15:46:11 +0100

    opus4 working

--- blocklySkini/scripts/hiphop_blocks.js
+++ blocklySkini/scripts/hiphop_blocks.js
@@ -767,8 +767,7 @@ Blockly.JavaScript['run_tank'] = function (block) {
 hh.RUN({
     "%location":{"filename":"","pos":1},
     "%tag":"run",
-    "module": `+ listTanks[i] + `,
-    {"filename":"","pos":2}),
+    "module": hh.getModule("`+ listTanks[i] + `", {"filename":"","pos":2}),
     "autocomplete":true
   }),
 
@@ -880,7 +879,7 @@ hh.LOCAL(
                 {
                   "%location":{"filename":"","pos":1},
                   "%tag":"run",
-                  "module": `+ listTanks[i] + `, {"filename":"","pos":2}),
+                  "module": hh.getModule("`+ listTanks[i] + `", {"filename":"","pos":2}),
                   "autocomplete":true,
                   "stopReservoir":"stop` + varRandom + `"
                 }
@@ -3867,7 +3866,7 @@ hh.LOCAL(
                 {
                   "%location":{"filename":"","pos":1},
                   "%tag":"run",
-                  "module": `+ listTanks[i] + `, {"filename":"","pos":2}),
+                  "module": hh.getModule("`+ listTanks[i] + `", {"filename":"","pos":2}),
                   "autocomplete":true,
                   "stopReservoir":"stop` + varRandom + `"
                 }
@@ -4024,7 +4023,7 @@ hh.LOCAL(
           {
             "%location":{"filename":"","pos":1},
             "%tag":"run",
-            "module": `+ listTanks[i] + `, {"filename":"","pos":2}),
+            "module": hh.getModule("`+ listTanks[i] + `", {"filename":"","pos":2}),
             "autocomplete":true,
             "stopReservoir":"stop` + varRandom + `"
           }
@@ -4184,7 +4183,7 @@ hh.LOCAL(
                 {
                   "%location":{"filename":"","pos":1},
                   "%tag":"run",
-                  "module": `+ listTanks[i] + `, {"filename":"","pos":2}),
+                  "module": hh.getModule("`+ listTanks[i] + `", {"filename":"","pos":2}),
                   "autocomplete":true,
                   "stopReservoir":"stop` + varRandom + `"
                 }
@@ -4847,7 +4846,7 @@ Blockly.JavaScript['hh_run'] = function (block) {
 hh.RUN({
   "%location":{},
   "%tag":"run",
-  "module": ` + modulehh + `,  // hh.getModule(     {}),
+  "module": hh.getModule(  "` + modulehh + `", {}),
   `
   for (var i = 0; i < listGroupes.length; i++) {
     code += `"` + listGroupes[i] + `":"",
