1bbc9a1298d4d2d77c2fc4bdc617a0fe8c91bc69
Bertrand Petit-Hédelin <bertrand@hedelin.fr>
2024-04-22 16:26:18 +0200

    HH ongoing

--- pieces/helloSkini.hh.js
+++ pieces/helloSkini.hh.js
@@ -2,6 +2,7 @@
 "use hopscript"
 
 import { ReactiveMachine } from "@hop/hiphop";
+import * as utilsSkini from "../serveur/utilsSkini.mjs";
 
 //let par;
 let midimix;
@@ -18,6 +19,14 @@ let tempoMax = 160;
 let tempoMin = 40;
 let tempoGlobal = 60;
 
+let tempoValue = 0;
+let tempoRythme = 0;
+let tempoLimit = 0;
+let tempoIncrease = true;
+let transposeValue = 0;
+let ratioTranspose = 1763 / 1000;
+let offsetTranspose = 635 / 10;
+
 let debug = false;
 let debug1 = true;
 
@@ -30,101 +39,51 @@ export function setServ(ser, daw, groupeCS, oscMidi, mix) {
   midimix = mix;
 }
 
-function setTempo(value) {
-  tempoGlobal = value;
-
-  if (midimix.getAbletonLinkStatus()) {
-    if (debug) console.log("ORCHESTRATION: set tempo Link:", value);
-    midimix.setTempoLink(value);
-    return;
-  }
-  if (value > tempoMax || value < tempoMin) {
-    console.log("ERR: Tempo set out of range:", value, "Should be between:", tempoMin, "and", tempoMax);
-    return;
-  }
-  let tempo = Math.round(127 / (tempoMax - tempoMin) * (value - tempoMin));
-  if (debug) {
-    console.log("Set tempo blockly:", value, par.busMidiDAW, CCChannel, CCTempo, tempo, oscMidiLocal.getMidiPortClipToDAW());
-  }
-  oscMidiLocal.sendControlChange(par.busMidiDAW, CCChannel, CCTempo, tempo);
-}
-
-let tempoValue = 0;
-let tempoRythme = 0;
-let tempoLimit = 0;
-let tempoIncrease = true;
-let transposeValue = 0;
-let ratioTranspose = 1763 / 1000;
-let offsetTranspose = 635 / 10;
-
-
-function moveTempo(value, limit) {
-  if (tempoLimit >= limit) {
-    tempoLimit = 0;
-    tempoIncrease = !tempoIncrease;
-  }
-
-  if (tempoIncrease) {
-    tempoGlobal += value;
-  } else {
-    tempoGlobal -= value;
-  }
-  if (debug) console.log("moveTempo:", tempoGlobal);
-  setTempo(tempoGlobal);
-  tempoLimit++;
-}
-
-function creationInterfaces(groupes) {
-  if (groupes !== undefined) {
-    return `out ${groupes.map(function (k) { 
-      console.log(k[0]);
-      return k[0] + "OUT";
-     })}; in ${groupes.map(function (k) { 
-      console.log(k[0]);
-      return k[0] + "IN";
-     })};
-    `
-   }
-}
-
 export function setSignals(param) {
   var i = 0;
-  let interText =  creationInterfaces(param.groupesDesSons);
-  console.log("inter:", interText);
+  let interTextOUT = utilsSkini.creationInterfacesOUT(param.groupesDesSons);
+  let interTextIN = utilsSkini.creationInterfacesIN(param.groupesDesSons);
+  console.log("inter:", interTextIN, interTextOUT);
 
-  hiphop interface groupes {out group1OUT, group2OUT; in group1IN, group2IN;};
-  //hiphop interface groupes interText;
-
-  const Program = hiphop module() implements groupes {
-  //const Program = hiphop module() {
+  const Program = hiphop module() {
     in A, B, R;
     out O, P;
     in start, halt, tick, DAWON, patternSignal, pulsation, midiSignal, emptyQueueSignal;
     in stopResevoir, stopMoveTempo;
+    out ... ${ interTextOUT };
+    in ... ${ interTextIN };
 
-    await (start.now);
-  
+    await(start.now);
+    host{utilsSkini.addSceneScore(1, serveur);}
+    host{utilsSkini.alertInfoScoreON("Skini HH", serveur);}
+
+    emit group1OUT([true, 0]);
+
     fork {
       abort(halt.now){
         every(tick.now){
-          host{ console.log("tick from HH 2", i++); }
+          host{
+            console.log("tick from HH", i++);
+            gcs.setTickOnControler(i);
+          }
         }
       }
+      emit group1OUT([false, 0]);
       host{ console.log("Reçu Halt"); }
     } par {
       do {
        fork {
-          await (A.now);
+          await(A.now);
           emit P();
         } par {
-          await (B.now);
+          await(B.now);
         }
        emit O();
        host{ console.log("aaaa"); }
       } every(R.now)
     }
   }
-  
+
   const prg = new ReactiveMachine(Program, "orchestration");
   return prg;
 }
---
+++ serveur/utilsSkini.mjs
@@ -0,0 +1,92 @@
+/**
+ * @fileOverview Utility functions for the scenarii written in hiphop.js
+ * @copyright (C) 2022-2024 Bertrand Petit-Hédelin
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * @author Bertrand Petit-Hédelin <bertrand@hedelin.fr>
+ * @version 1.4
+ */
+
+"use strict"
+"use hopscript"
+
+import { ReactiveMachine } from "@hop/hiphop";
+
+export function creationInterfacesOUT(groupes) {
+  if (groupes !== undefined) {
+    return groupes.map(function (k) {
+      return k[0] + "OUT";
+    })
+  };
+}
+
+export function creationInterfacesIN(groupes) {
+  if (groupes !== undefined) {
+    return groupes.map(function (k) {
+      return k[0] + "IN";
+    })
+  };
+}
+
+export function setTempo(value) {
+  tempoGlobal = value;
+
+  if (midimix.getAbletonLinkStatus()) {
+    if (debug) console.log("ORCHESTRATION: set tempo Link:", value);
+    midimix.setTempoLink(value);
+    return;
+  }
+  if (value > tempoMax || value < tempoMin) {
+    console.log("ERR: Tempo set out of range:", value, "Should be between:", tempoMin, "and", tempoMax);
+    return;
+  }
+  let tempo = Math.round(127 / (tempoMax - tempoMin) * (value - tempoMin));
+  if (debug) {
+    console.log("Set tempo blockly:", value, par.busMidiDAW, CCChannel, CCTempo, tempo, oscMidiLocal.getMidiPortClipToDAW());
+  }
+  oscMidiLocal.sendControlChange(par.busMidiDAW, CCChannel, CCTempo, tempo);
+}
+
+export function moveTempo(value, limit) {
+  if (tempoLimit >= limit) {
+    tempoLimit = 0;
+    tempoIncrease = !tempoIncrease;
+  }
+
+  if (tempoIncrease) {
+    tempoGlobal += value;
+  } else {
+    tempoGlobal -= value;
+  }
+  if (debug) console.log("moveTempo:", tempoGlobal);
+  setTempo(tempoGlobal);
+  tempoLimit++;
+}
+
+export function addSceneScore(message, serveur) {
+  var msg = {
+    type: 'addSceneScore',
+    value: ` + number + `
+  }
+  serveur.broadcast(JSON.stringify(msg));
+}
+
+export function alertInfoScoreON(message, serveur) {
+  var msg = {
+    type: 'alertInfoScoreON',
+    value: message
+  }
+  serveur.broadcast(JSON.stringify(msg));
+}
